ee6e7e321c06e28b8b7d6f1b1378c4d2a5985add
leeight <leeight@gmail.com>
2016-10-07 09:33:18 +0800

    Add x86_64 support

--- jsc/extra_headers/LLIntAssembly.h
+++ jsc/extra_headers/LLIntAssembly.h
@@ -4,6 +4,10 @@
 #include "LLIntAssembly.android-armv7.h"
 #elif defined(__ANDROID__) && defined(__i386__)
 #include "LLIntAssembly.android-x86.h"
+#elif defined(__ANDROID__) && defined(__x86_64__)
+#include "LLIntAssembly.android-x86_64.h"
+#elif defined(__ANDROID__) && defined(__aarch64__)
+#include "LLIntAssembly.android-arm64.h"
 #else
 #error "Platform not supported"
 #endif
