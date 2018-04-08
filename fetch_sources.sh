83ba9b4fda0a1ce800508544216cc766c5b6a297
gengjiawen <technicalcute@gmail.com>
2018-04-08 09:05:26 +0800

    fix add source

--- fetch_sources.sh
+++ fetch_sources.sh
@@ -12,9 +12,6 @@ curl -o icu4c.tar.gz https://android.googlesource.com/platform/external/icu/+arc
 echo "Extracting ICU"
 tar -zxf icu4c.tar.gz -C icu
 
-echo "Downloading JSC"
-curl -O https://builds-nightly.webkit.org/files/trunk/src/WebKit-r174650.tar.bz2
-
-echo "Extracting JSC"
-tar -jxf WebKit-r174650.tar.bz2 -C jsc --strip 2 WebKit-r174650/Source/JavaScriptCore WebKit-r174650/Source/WTF
+echo "set JSC"
+cp -r ~/webkit/Source/JavaScriptCore WebKit-r174650/Source/WTF
 
